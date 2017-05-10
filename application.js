b1886c2fdf0cfec1afdcbefcb43ccd37d1216008
David Feroli <daferoli@gmail.com>
2017-05-10 00:01:41 -0400

    added department mongo connections

--- application.js
+++ application.js
@@ -7,6 +7,7 @@ app.use(bodyParser.json());
 
 app.use('/employee', require('lib/employee'));
 app.use('/office', require('lib/office'));
+app.use('/department', require('lib/department'));
 
 app.get('/', function(req, res){
   res.send('THIS IS A TEST');
